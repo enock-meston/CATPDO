1cff84d2795fb55dda06b001343fd27eb3932d91
eric-dj123 <105584076+eric-dj123@users.noreply.github.com>
2022-05-22 01:10:48 -0700

    change f

--- user/includes/footer.php
+++ user/includes/footer.php
@@ -1,8 +1,8 @@
 <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; nigoote company 2022</span><br>
-                        <span>Contuct: 0783 982 872</span>
+                        <span>Copyright &copy; ndikumana Eric and Ndagijimana Enock</span><br>
+                        <span>Contact: 0783 982 872 && 0782185745</span>
                     </div>
                 </div>
             </footer>
--- user/includes/sidebar.php
+++ user/includes/sidebar.php
@@ -5,7 +5,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <!-- <i class="fas fa-laugh-wink"></i> -->
         </div>
-        <div class="sidebar-brand-text mx-3">Checkout <sup>Tourism</sup></div>
+        <div class="sidebar-brand-text mx-3">Rwanda Tourism  <sup>Company</sup></div>
     </a>
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
