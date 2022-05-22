e5c005968293e87b306d8c795bab1aebad571ba5
eric-dj123 <105584076+eric-dj123@users.noreply.github.com>
2022-05-22 01:54:15 -0700

    change  user view

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,8 +1,8 @@
 <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; nigoote.com company 2022</span><br>
-                        <span>Contuct: 0783 982 872</span>
+                        <span>Copyright &copy; Ndikumana Eric And Ndagijimana Enock</span><br>
+                        <span>Contact: 0782185745 && 0783922872</span>
                     </div>
                 </div>
             </footer>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <!-- <i class="fas fa-laugh-wink"></i> -->
         </div>
-        <div class="sidebar-brand-text mx-3">Kibihekane tvet <sup> tour company</sup></div>
+        <div class="sidebar-brand-text mx-3"> <sup>Rwnada Tourism company</sup></div>
     </a>
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
