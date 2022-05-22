3d8e093ce075940879258c4eeb8c59e566a20fd5
eric-dj123 <105584076+eric-dj123@users.noreply.github.com>
2022-05-22 09:18:13 -0700

    update foooter

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,7 +2,7 @@
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
                         <span>Copyright &copy; Ndikumana Eric And Ndagijimana Enock</span><br>
-                        <span>Contact: 0782185745 && 0783922872</span>
+                        <span>Contact: 07821845 && 0783922872</span>
                     </div>
                 </div>
             </footer>
